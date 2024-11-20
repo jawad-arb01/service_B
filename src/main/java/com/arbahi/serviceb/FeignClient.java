e68f6149b1a80cfb93309d9a3fd3013ee646e319
ARBAHI Jawad <106961582+jawad-arb@users.noreply.github.com>
2024-11-20 16:11:36 +0100

    update the eureka server method

--- src/main/java/com/arbahi/serviceb/FeignClient.java
+++ src/main/java/com/arbahi/serviceb/FeignClient.java
@@ -2,7 +2,7 @@ package com.arbahi.serviceb;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
-@org.springframework.cloud.openfeign.FeignClient(name = "service-a",url = "https://servicea001.azurewebsites.net")
+@org.springframework.cloud.openfeign.FeignClient(name = "service-a",url = "${service.a.url}")
 public interface FeignClient {
     @GetMapping("/api/service-a/greet")
     String getGreeting();
