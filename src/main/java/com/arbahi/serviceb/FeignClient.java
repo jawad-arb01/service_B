4dfd4d1feec9a6933846dd8504c73d900b03c62f
jawad-arb01 <jawad.arbahi@etu.uae.ac.ma>
2024-11-20 15:20:29 +0100

    Update FeignClient.java

--- src/main/java/com/arbahi/serviceb/FeignClient.java
+++ src/main/java/com/arbahi/serviceb/FeignClient.java
@@ -2,7 +2,7 @@ package com.arbahi.serviceb;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
-@org.springframework.cloud.openfeign.FeignClient(name = "service-a")
+@org.springframework.cloud.openfeign.FeignClient(name = "service-a", ,url = "https://servicea001.azurewebsites.net")
 public interface FeignClient {
     @GetMapping("/api/service-a/greet")
     String getGreeting();
